c732b68971f4dc8fdc5d93b8cba762eafd644380
zhangboyuan <279202355@qq.com>
2014-05-21 20:54:01 +0800

    BDD

---
+++ BDD/test/ngpanwei/bdd/stepdefs/zzz.java
@@ -0,0 +1,27 @@
+package ngpanwei.bdd.stepdefs;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class zzz {
+	@Given("^Backlog 有 任务\"(.*?)\"$")
+	public void backlog_有_任务(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^我完成\"(.*?)\"$")
+	public void 我完成(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^未完成任务是\"(.*?)\"$")
+	public void 未完成任务是(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
